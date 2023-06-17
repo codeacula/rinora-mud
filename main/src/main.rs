3de0c742c6c66869d080a84daeea7b8102008ed3
Codeacula <codeacula@codeacula.com>
2023-06-17 09:59:27 -0500

    No more rust format, trying to make the app run

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,14 +1,12 @@
-use bevy::{
-  app::{ScheduleRunnerPlugin, ScheduleRunnerSettings},
-  prelude::*,
-  utils::Duration,
-};
+use bevy::{app::ScheduleRunnerSettings, prelude::*, utils::Duration};
 use server::*;
 
 fn main() {
-  let desired_loop = ScheduleRunnerSettings::run_loop(Duration::from_secs_f64(1.0 / 60.0));
-  App::new()
-    .add_plugins(MinimalPlugins.set(desired_loop))
-    .add_plugin(ServerPlugin)
-    .run();
+    App::new()
+        .insert_resource(ScheduleRunnerSettings::run_loop(Duration::from_secs_f64(
+            1.0 / 60.0,
+        )))
+        .add_plugins(MinimalPlugins)
+        .add_plugin(ServerPlugin)
+        .run();
 }
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,20 +1,20 @@
 pub struct ServerPlugin;
 
 use std::{
-  io::Read,
-  net::{TcpListener, TcpStream},
+    io::Read,
+    net::{TcpListener, TcpStream},
 };
 
 use bevy::{ecs::system::Commands, prelude::*};
 
 #[derive(Component)]
 struct Connection {
-  stream: TcpStream,
+    stream: TcpStream,
 }
 
 #[derive(Resource)]
 struct Server {
-  listener: TcpListener,
+    listener: TcpListener,
 }
 
 #[derive(Component)]
@@ -23,65 +23,61 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
-
 fn start_listening(mut commands: Commands) {
-  let listener = match TcpListener::bind("0.0.0.0:23") {
-    Ok(listener) => listener,
-    Err(e) => panic!("{:?}", e),
-  };
+    let listener = match TcpListener::bind("0.0.0.0:23") {
+        Ok(listener) => listener,
+        Err(e) => panic!("{:?}", e),
+    };
 
-  let server = Server { listener };
-  commands.insert_resource(server);
+    let server = Server { listener };
+    commands.insert_resource(server);
 }
 
 fn check_for_new_connections(mut commands: Commands, server: Res<Server>) {
-  for conn in server.listener.incoming() {
-    match conn {
-      Ok(stream) => {
-        commands.spawn((Connection { stream },));
-        commands.spawn((Person, Name("Elaina Proctor".to_string())));
-        println!("New connection!");
-      }
-      Err(err) => println!("Um, err? {}", err),
+    for conn in server.listener.incoming() {
+        match conn {
+            Ok(stream) => {
+                commands.spawn((Connection { stream },));
+                commands.spawn((Person, Name("Elaina Proctor".to_string())));
+                println!("New connection!");
+            }
+            Err(err) => println!("Um, err? {}", err),
+        }
     }
-  }
 }
 
 fn check_for_waiting_input(all_connections: Query<&Connection>) {
-  for conn in all_connections.iter() {
-    let mut buf = String::new();
-    let mut stream_copy = match conn.stream.try_clone() {
-      Ok(stream) => stream,
-      Err(e) => panic!("{:?}", e),
-    };
+    for conn in all_connections.iter() {
+        let mut buf = String::new();
+        let mut stream_copy = match conn.stream.try_clone() {
+            Ok(stream) => stream,
+            Err(e) => panic!("{:?}", e),
+        };
 
-    match stream_copy.read_to_string(&mut buf) {
-        Err(err) => {
-          panic!("{:?}", err);
+        match stream_copy.read_to_string(&mut buf) {
+            Err(err) => {
+                panic!("{:?}", err);
+            }
+            _ => (),
         }
-        _ => (),
-    }
 
-    if buf.len() > 0 {
-      println!("Received: {}", buf);
+        if buf.len() > 0 {
+            println!("Received: {}", buf);
+        }
     }
-  }
 }
 
 fn greet_people(query: Query<&Name, With<Person>>) {
-  for name in &query {
-      println!("hello {}!", name.0);
-  }
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
 }
 
-
 impl Plugin for ServerPlugin {
-  fn build(&self, app: &mut App) {
-    app
-      .add_startup_system(start_listening)
-      .add_system(check_for_new_connections)
-      .add_system(check_for_waiting_input)
-      .add_system(greet_people)
-      ;
-  }
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(start_listening)
+            .add_system(check_for_new_connections)
+            .add_system(check_for_waiting_input)
+            .add_system(greet_people);
+    }
 }
