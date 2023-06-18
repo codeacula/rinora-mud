1e175194cb8f1cd9afdb6c8d33233245633ca5ff
Codeacula <codeacula@codeacula.com>
2023-06-18 08:28:39 -0500

    Gonna take a break

--- main/src/lib.rs
+++ main/src/lib.rs
@@ -7,16 +7,21 @@ use bevy::{
 };
 use server::ServerPlugin;
 
+pub fn hello_us() {
+    println!("hello us!");
+}
+
 /// Sets up the application to run correctly
 pub fn init_app(app: &mut App) -> &mut App {
     app.add_plugin(LogPlugin {
         level: Level::DEBUG,
-        filter: "bevy_render=trace,bevy_ecs=trace".to_string(),
+        filter: "bevy_ecs=trace".to_string(),
     })
     .insert_resource(ScheduleRunnerSettings::run_loop(Duration::from_secs_f64(
         1.0 / 60.0,
     )))
     .add_plugins(MinimalPlugins)
+    .add_system(hello_us)
     .add_plugin(ServerPlugin);
     return app;
 }
