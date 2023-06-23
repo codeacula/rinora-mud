686888f09d7dac87454588c5685a1f18a147192c
Codeacula <codeacula@codeacula.com>
2023-06-22 22:18:37 -0500

    About to try a channel

--- main/src/lib.rs
+++ main/src/lib.rs
@@ -1,6 +1,4 @@
-use std::env;
-
-use tokio::net::TcpListener;
+use std::{env, net::TcpListener, sync::mpsc, thread};
 
 pub struct GameServer {
     listener: Option<TcpListener>,
@@ -12,18 +10,32 @@ impl GameServer {
     }
 
     pub async fn start_server(&mut self) {
-        let server_host = env::var("SERVER_HOST").unwrap_or(String::from("0.0.0.0"));
-        let server_port = env::var("SERVER_PORT").unwrap_or(String::from("23"));
+        let (tx, rx) = mpsc::channel();
+        thread::spawn(move || {
+            let server_host = env::var("SERVER_HOST").unwrap_or(String::from("0.0.0.0"));
+            let server_port = env::var("SERVER_PORT").unwrap_or(String::from("23"));
 
-        let listener = TcpListener::bind(format!("{}:{}", server_host, server_port))
-            .await
-            .unwrap();
-        self.listener = Some(listener);
+            let listener = match TcpListener::bind(format!("{}:{}", server_host, server_port)) {
+                Ok(listener) => listener,
+                Err(e) => {
+                    panic!("Error starting TCP listener: {}", e);
+                }
+            };
+
+            for conn in listener.incoming() {
+                match conn {
+                    Ok(conn) => {
+                        tx.send(conn).unwrap();
+                    }
+                    Err(e) => {
+                        panic!("Error accepting connection: {}", e);
+                    }
+                }
+            }
+        });
     }
 
     pub async fn start_game_loop(&mut self) {
-        loop {
-            self.listener.as_ref().unwrap().poll_accept(_a);
-        }
+        loop {}
     }
 }
--- main/src/main.rs
+++ main/src/main.rs
@@ -3,7 +3,5 @@ use tokio;
 
 #[tokio::main]
 async fn main() {
-    let mut game_server = GameServer::new();
-    game_server.start_server().await;
-    game_server.start_game_loop();
+    let app = App::new();
 }
