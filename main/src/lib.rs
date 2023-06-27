6b79f3dcf33388f06e302f5afd8488ed61c6951b
Codeacula <codeacula@codeacula.com>
2023-06-27 17:32:09 -0500

    I see a way forward

--- main/src/lib.rs
+++ main/src/lib.rs
@@ -1,40 +1,21 @@
-mod game_server;
 
-use bevy_ecs::prelude::*;
-use game_server::GameServer;
+use std::time::Duration;
+
+use bevy::{prelude::*, log::{LogPlugin, Level}, app::ScheduleRunnerSettings};
+use server::GameServer;
 
 pub fn start_game() {
-    let mut world = World::new();
+    let mut app = App::new();
 
-    let server = GameServer::new();
-
-    let mut schedule = Schedule::default();
-
-    let connection_activity_listener = server.connection_event_listener;
-
-    println!("Starting game loop");
-    loop {
-        let connection_event = match connection_activity_listener.try_recv() {
-            Err(_) => None,
-            Ok(conn) => Some(conn),
-        };
-
-        if connection_event.is_some() {
-            let event_info = connection_event.unwrap();
-
-            match event_info.event_type {
-                game_server::ConnectionEventTypes::ConnectionDropped {} => {
-                    println!("Connection dropped");
-                },
-                game_server::ConnectionEventTypes::DataReceived {} => {
-                    println!("Got data");
-                },
-                game_server::ConnectionEventTypes::NewConnection {} => {
-                    println!("New connection");
-                },
-            }
-        }
-
-        schedule.run(&mut world);
-    }
+    app
+        .add_plugin(LogPlugin {
+            level: Level::DEBUG,
+            filter: "bevy_ecs=trace".to_string(),
+        })
+        .insert_resource(ScheduleRunnerSettings::run_loop(Duration::from_secs_f64(
+            1.0 / 60.0,
+        )))
+        .add_plugins(MinimalPlugins)
+        .add_plugin(GameServer)
+        .run()
 }
