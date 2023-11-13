10f0a1cc09b7142f6747ed0a5860bb61f19e7f8a
Codeacula <codeacula@codeacula.com>
2023-11-13 09:00:02 -0600

    Trying to handle character movement

--- base/src/commands/mod.rs
+++ base/src/commands/mod.rs
@@ -1,6 +1,7 @@
 pub mod confirm_account_password;
 pub mod create_account_password;
 pub mod create_character_command;
+pub mod move_to_room;
 pub mod provide_account_password;
 pub mod provide_character_name;
 pub mod provide_username;
@@ -10,6 +11,7 @@ pub mod prelude {
     pub use crate::commands::confirm_account_password::*;
     pub use crate::commands::create_account_password::*;
     pub use crate::commands::create_character_command::*;
+    pub use crate::commands::move_to_room::*;
     pub use crate::commands::provide_account_password::*;
     pub use crate::commands::provide_character_name::*;
     pub use crate::commands::provide_username::*;
