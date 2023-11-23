c300e6a4c5975b66945f0fd4fd966147d4974b5d
Codeacula <codeacula@codeacula.com>
2023-11-22 18:35:17 -0600

    Working example

--- base/src/stream_processor.rs
+++ base/src/stream_processor.rs
@@ -1,11 +1,10 @@
-use bevy::utils::Uuid;
-
-use crate::{enums::*, gmcp::*, models::*};
+use crate::{gmcp::*, models::*};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum NetworkCommandType {
     User,
     Gmcp,
+    TurnOnGmcp,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -16,26 +15,19 @@ pub struct NetworkCommand {
 }
 
 pub trait Step {
-    fn next(&mut self, byte: u8) -> (Box<dyn Step>, Option<NetworkEvent>);
+    fn next(&mut self, byte: u8) -> (Box<dyn Step>, Option<NetworkCommand>);
 }
 
 pub struct BufferProcessor {
-    current_command: Option<NetworkEvent>,
     current_step: Box<dyn Step>,
 }
 
 impl BufferProcessor {
-    fn next(&mut self, byte: u8) -> Option<NetworkEvent>{
-        (next_step, command_result) = self.current_step.next(byte);
+    pub fn next(&mut self, byte: u8) -> Option<NetworkCommand> {
+        let (next_step, command_result) = self.current_step.next(byte);
 
         self.current_step = next_step;
-        None
-    }
-
-    fn get_current_command(mut self) -> Option<NetworkEvent> {
-        let command_to_send = self.current_command;
-        self.current_command = None;
-        command_to_send
+        command_result
     }
 }
 
@@ -43,7 +35,7 @@ impl BufferProcessor {
 struct InitialState {}
 
 impl Step for InitialState {
-    fn next(&mut self, processor: &mut BufferProcessor, byte: u8) ->  (Box<dyn Step>, Option<NetworkEvent>) {
+    fn next(&mut self, byte: u8) -> (Box<dyn Step>, Option<NetworkCommand>) {
         match byte {
             IAC => (Box::new(IACState {}), None),
             _ => (Box::new(InitialState {}), None),
@@ -55,7 +47,7 @@ impl Step for InitialState {
 struct IACState {}
 
 impl Step for IACState {
-    fn next(&mut self, processor: &mut BufferProcessor, byte: u8) ->  (Box<dyn Step>, Option<NetworkEvent>) {
+    fn next(&mut self, byte: u8) -> (Box<dyn Step>, Option<NetworkCommand>) {
         match byte {
             DO => (Box::new(DoState {}), None),
             _ => (Box::new(InitialState {}), None),
@@ -67,35 +59,43 @@ impl Step for IACState {
 struct DoState {}
 
 impl Step for DoState {
-    fn next(&mut self, processor: &mut BufferProcessor, byte: u8) ->  (Box<dyn Step>, Option<NetworkEvent>) {
+    fn next(&mut self, byte: u8) -> (Box<dyn Step>, Option<NetworkCommand>) {
         match byte {
-            GMCP => (Box::new(InitialState {}), None)
-            _ => (Box::new(InitialState {}), None)
+            GMCP => (
+                Box::new(InitialState {}),
+                Some(NetworkCommand {
+                    command_type: NetworkCommandType::TurnOnGmcp,
+                    command_name: String::from(""),
+                    data: None,
+                }),
+            ),
+            _ => (Box::new(InitialState {}), None),
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::net::{TcpStream, TcpListener};
-
     use super::*;
-    use crate::enums::NetworkEventType;
 
     #[test]
     fn processor_switched_to_iac_state_from_inital() {
         let mut processor = BufferProcessor {
-            current_command: None,
             current_step: Box::new(InitialState {}),
         };
 
         processor.next(IAC);
         processor.next(DO);
-        processor.next(GMCP);
-
-        let command = processor.get_current_command();
+        let command = processor.next(GMCP);
 
         assert!(command.is_some());
-        assert_eq!(command.unwrap().event_type, NetworkEventType::GmcpReceived);
+        assert_eq!(
+            command.unwrap(),
+            NetworkCommand {
+                command_type: NetworkCommandType::TurnOnGmcp,
+                command_name: String::from(""),
+                data: None
+            }
+        );
     }
 }
