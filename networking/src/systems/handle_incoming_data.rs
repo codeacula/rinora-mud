6097108f74e770bd271698d99f01230ac4963269
Codeacula <codeacula@codeacula.com>
2023-12-01 07:00:30 -0600

    handle_incoming_data is now fully tested

--- networking/src/systems/handle_incoming_data.rs
+++ networking/src/systems/handle_incoming_data.rs
@@ -185,4 +185,29 @@ mod tests {
         );
         drop(server);
     }
+
+    #[test]
+    fn it_turns_on_room() {
+        let (server, read_handle, mut write_handle, inc_send, _inc_recv) = setup();
+
+        // Tell the server to turn on GMCP
+        write_handle.write_all(&[IAC, SB, GMCP]).unwrap();
+        write_handle
+            .write_all(String::from("Core.Supports.Set [ \"Room 1\" ]").as_bytes())
+            .unwrap();
+        write_handle.write_all(&[IAC, SE]).unwrap();
+
+        // Get the connection from the listener
+
+        let mut test_connection = NetworkConnection {
+            id: Uuid::new_v4(),
+            conn: read_handle,
+            gmcp: false,
+            do_room: false,
+        };
+
+        handle_incoming_data(&mut test_connection, &inc_send);
+        assert!(test_connection.do_room);
+        drop(server);
+    }
 }
