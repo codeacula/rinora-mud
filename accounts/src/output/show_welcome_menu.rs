1459dadb89514541900010d72507aaeb3420a7ad
Codeacula <codeacula@codeacula.com>
2023-12-02 13:24:14 -0600

    wip

--- accounts/src/output/show_welcome_menu.rs
+++ accounts/src/output/show_welcome_menu.rs
@@ -1,3 +1,4 @@
+use database::prelude::*;
 use shared::prelude::*;
 
 use crate::events::WelcomeUserEvent;
@@ -6,14 +7,45 @@ pub(crate) fn show_welcome_menu(
     mut welcome_user_rx: EventReader<WelcomeUserEvent>,
     mut text_event_tx: EventWriter<TextEvent>,
     mut show_prompt_event_tx: EventWriter<ShowPromptEvent>,
+    db_interface: Res<DbInterface>,
+    query: Query<&User>,
 ) {
     for WelcomeUserEvent(entity) in welcome_user_rx.read() {
-        text_event_tx.send(TextEvent::from_str(
-            *entity,
-"{{15}}Welcome to RinoraMUD!\n{{7}}Either provide the character you want to play, or select an option from below:
-  1. Create a new character",
-        ));
+        let user = match query.get(*entity) {
+            Ok(user) => user,
+            Err(_) => {
+                text_event_tx.send(TextEvent::send_generic_error(*entity));
+                error!("User {:?} not found", entity);
+                continue;
+            }
+        };
 
+        let mut output = String::new();
+
+        output.push_str(
+            "{{15}}Welcome to RinoraMUD!
+        {{7}}Either provide the character you want to play, or select an option from below:
+          1. Create a new character",
+        );
+
+        let characters = match db_interface.characters.get_all_by_user(user.id) {
+            Ok(characters) => characters,
+            Err(err) => {
+                text_event_tx.send(TextEvent::send_generic_error(*entity));
+                error!("Error getting characters for user {}: {}", user.id, err);
+                continue;
+            }
+        };
+
+        if characters.len() > 0 {
+            output.push_str("\nYour characters:{{15}}\n");
+
+            for character in characters {
+                output.push_str(format!("  {}\n", character.display_name.0).as_str());
+            }
+        }
+
+        text_event_tx.send(TextEvent::from_str(*entity, output.as_str()));
         show_prompt_event_tx.send(ShowPromptEvent(*entity));
     }
 }
